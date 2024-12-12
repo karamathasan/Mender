ec5dc16523de0b4313cd516e7e39b70740ce3f3e
karamathasan <94758689+karamathasan@users.noreply.github.com>
2024-12-12 09:52:37 -0500

    created point2D class

--- rendering/triangulation.py
+++ rendering/triangulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from elements._2D.point import Point2D
 class Triangulator():
     def __init__():
         pass
@@ -6,7 +7,23 @@ class Triangulator():
         pass
     
 class Delauney(Triangulator):
-    def __init__():
+    def __init__(self, points: list[Point2D]):
+        self.points = points
+
+    def triangulate(self):
+        """
+        Calculate the lines that would be needed to be drawn with pygame to create a delauney triangulation of the available points
+        """
         pass
-    def triangulate(self, points: list[np.ndarray]):
+
+class Voronoi():
+    def __init__(self, points: list[Point2D]):
+        self.points = points 
+ 
+    def render(self, pxarray):
+        """
+        Calculate the the pixel coordinates of the points that are equally distant to the points in this Voronoi renderer
+        """
+        # Iterate through each pixel in pxarray, convert px array coordinates into world coordinates, calculate the distances
+        # O(mn * p)
         pass
